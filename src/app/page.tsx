fe1f5e23c4f7bab0672679c59c82398cba5abad7
clara <baskvava@gmail.com>
2023-12-03 18:19:20 -0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Welcome from "@/components/Welcome";
+import { Theme } from "@radix-ui/themes";
+import { ThemeProvider } from "next-themes";
+
+export default function Home() {
+  return (
+    <ThemeProvider attribute="class">
+      <Theme>
+        <Welcome />
+      </Theme>
+    </ThemeProvider>
+  );
+}
